f007ea23f6a26bbea975c67152d30189e7d4447d
will.bi <will.bi@innocellence.com>
2020-10-12 21:45:49 +0800

    加入通用方法，数据列表的序号从1开始排

---
+++ Infrastructure/Infrastructure.Web.Domain/Model/ViewModel/DLYB/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Web.Domain.ModelsView
+{
+    public class ViewModelBase
+    {
+        public virtual int Index { get; set; }
+    }
+}
