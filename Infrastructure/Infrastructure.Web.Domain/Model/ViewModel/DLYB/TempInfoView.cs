90481abbd26e19dddbbb72b36f456b56a3631682
Summer Zhang <summer.zhang@innocellence.com>
2020-11-11 01:11:54 +0800

    添加梁段信息 查询

--- Infrastructure/Infrastructure.Web.Domain/Model/ViewModel/DLYB/TempInfoView.cs
+++ Infrastructure/Infrastructure.Web.Domain/Model/ViewModel/DLYB/TempInfoView.cs
@@ -9,44 +9,43 @@ namespace Infrastructure.Web.Domain.ModelsView
     public partial class TempInfoView : ViewModelBase, IViewModel
 	{
         public Int32 Id { get; set; }
-        public long ProjectId { get; set; }
+        public string ProjectId { get; set; }
         public string ProjectName { get; set; }
+        public string BeamName { get; set; }
         public string AffiliatedInstitution { get; set; }
-        public long AffiliatedId { get; set; }
+        public string AffiliatedId { get; set; }
         public string BridgeType { get; set; }
-        public long BridgeTypeId { get; set; }
-        public long FileId { get; set; }
+        public string BridgeTypeId { get; set; }
+        public string FileId { get; set; }
         public string FileStatus { get; set; }
-        public long RelationId { get; set; }
+        public string RelationId { get; set; }
         public string RelationFigureId { get; set; }
         public string FigureNumber { get; set; }
         public string BoardNumber { get; set; }
-        public double Thickness { get; set; }
+        public string BarNumber { get; set; }
+        public double? Thickness { get; set; }
         //焊缝数量
-        public double WeldNum { get; set; }
+        public double? WeldNum { get; set; }
         //梁段数量
-        public double BeamNum { get; set; }
-        public double LengthVal { get; set; }
-        public double WidthVal { get; set; }
-        public DateTime? CreatedDate { get; set; }
+        public double? BeamNum { get; set; }
+        public double? LengthVal { get; set; }
+        public double? WidthVal { get; set; }
+       // public DateTime? CreatedDate { get; set; }
 
-        public int? CreatedUserID { get; set; }
+        //public int? CreatedUserID { get; set; }
 
-        public DateTime? UpdatedDate { get; set; }
+        //public DateTime? UpdatedDate { get; set; }
 
-        public int? UpdatedUserID { get; set; }
+        //public int? UpdatedUserID { get; set; }
 
-        public bool IsDeleted { get; set; }
-
-        public string HandleID { get; set; }
-
-        public int? CopyOriginId { get; set; }
-
+        //public bool IsDeleted { get; set; }
+        
         public IViewModel ConvertAPIModel(object obj) {
             var entity = (TempInfo)obj;
             Id = entity.Id;
             ProjectId = entity.ProjectId;
             ProjectName = entity.ProjectName;
+            BeamName = entity.BeamName;
             AffiliatedInstitution = entity.AffiliatedInstitution;
             AffiliatedId = entity.AffiliatedId;
             BridgeType = entity.BridgeType;
---
+++ UI/Innocellence.Web/Controllers/TempInfoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Infrastructure.Web.Domain.Entity;
+using Infrastructure.Web.Domain.Model;
+using Innocellence.Web.Models;
+using Infrastructure.Core.Data;
+using Infrastructure.Web.Domain.Contracts;
+using Infrastructure.Web.Domain.ModelsView;
+using Infrastructure.Web.UI;
+using Infrastructure.Utility.Filter;
+using System.Linq.Expressions;
+using DLYB.Web.Controllers;
+using Infrastructure.Web.Domain.Services;
+using Infrastructure.Web.Domain.Common;
+using Infrastructure.Utility.Extensions;
+
+namespace Innocellence.Web.Controllers
+{
+    public class TempInfoController : BaseController<TempInfo, TempInfoView>
+    {
+        private const string SLASH = "/";
+        private readonly ITempInfoService _TempInfoService;
+        private readonly IProjectService _projectService;
+        private readonly ITaskListService _taskListService;
+
+        public TempInfoController(ITempInfoService TempInfoService, IProjectService projectService,
+            ITaskListService taskListService) : base(TempInfoService)
+        {
+            _TempInfoService = TempInfoService;
+            _projectService = projectService;
+            _taskListService = taskListService;
+        }
+        // GET: Address
+        public override ActionResult Index()
+        {
+            return View();
+        }
+        
+        public override ActionResult GetList()
+        {
+            GridRequest gridRequest = new GridRequest(Request);
+            string strCondition = Request["search_condition"];
+           
+            Expression<Func<TempInfo, bool>> expression = FilterHelper.GetExpression<TempInfo>(gridRequest.FilterGroup);
+           
+         
+           if (!string.IsNullOrEmpty(strCondition))
+            {
+                expression = expression.AndAlso<TempInfo>(x => x.BeamName.Contains(strCondition)||
+                x.BoardNumber.Contains(strCondition)||x.ProjectName.Contains(strCondition)||x.FigureNumber.Contains(strCondition));
+            }
+         
+            int rowCount = gridRequest.PageCondition.RowCount;
+            List<TempInfoView> listEx = GetListEx(expression, gridRequest.PageCondition);
+           
+            return this.GetPageResult(listEx, gridRequest);
+        }
+      
+    }
+}
